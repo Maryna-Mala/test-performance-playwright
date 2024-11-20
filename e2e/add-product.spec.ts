24facf757d3f3376539a026016d44ee726670053
Maryna Mala <marinamalaa0@gmail.com>
2024-11-20 21:08:14 +0200

    test cases added

---
+++ e2e/add-product.spec.ts
@@ -0,0 +1,21 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Login with valid credentials for your free account', async () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://idealistcoffee.com/');
+        await page.locator('[data-role="customer-icon"]').click();
+        await page.locator('#email').fill('marinamalaa0@gmail.com');
+        await page.locator('[name="login[password]"]').fill('Testuser1234');
+        await page.locator("//button[contains(@class, 'primary')]").click();
+    });
+
+    test('Add Brazil Espresso 250 grams to the cart', async ({ page }) => {
+        //click on header on the home page
+        await page.locator("//li[contains(@class, 'nav-1')]").click()
+        //the first item on the page is visible
+        await expect(page.locator("//span[contains(text(), 'Coffee Tonic Idealist 0,33 л')]")).toBeVisible()
+        //adding it to the cart
+        await page.locator('[data-product-sku="4650"]').click()
+    });
+
+});
--- e2e/login.spec.ts
+++ e2e/login.spec.ts
@@ -3,13 +3,12 @@ import { test, expect } from '@playwright/test';
 test.describe('Idealist login', async () => {
     test.beforeEach(async ({ page }) => {
         await page.goto('https://idealistcoffee.com/');
-        await expect(page).toHaveTitle(/Idealist Coffee/);
         await page.locator('[data-role="customer-icon"]').click();
         await page.locator('#email').fill('marinamalaa0@gmail.com');
     });
 
     test('Login with valid credentials for your free account', async ({ page }) => {
-        await page.locator('#pass').fill('Testuser1234');
+        await page.locator('[name="login[password]"]').fill('Testuser1234');
         await page.locator("//button[contains(@class, 'primary')]").click();
 
         const profileIcon = page.locator('.icon-account');
@@ -17,7 +16,7 @@ test.describe('Idealist login', async () => {
     });
 
     test('Login with invalid credentials for your free account', async ({ page }) => {
-        await page.locator('#pass').fill('Testuser12345');
+        await page.locator('[name="login[password]"]').fill('Testuser12345');
         await page.locator("//button[contains(@class, 'primary')]").click();
         const errorMessage = await page.locator('[data-ui-id="message-error"]').textContent(); 
 
