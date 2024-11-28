0a9b2f61ee58af80468e6917316fab5c7099075e
Maryna Mala <marinamalaa0@gmail.com>
2024-11-28 20:29:39 +0200

    fixed issues

--- tests/add-product.spec.ts
+++ tests/add-product.spec.ts
@@ -1,10 +1,10 @@
 import { expect, test } from '../src/FixtureConfig.ts'
 
-    test.only('Add the first product item to the cart', async ({ page, homePage, coffeePage }) => {
-        await page.goto('')
-        expect(page.url()).toContain(await homePage.getPageUrl());
-        await homePage.header.clickOnFirstElement()
-        await expect (coffeePage.productItem.productItemList()).toContainText('Coffee Tonic')
-        await coffeePage.productItem.clickAddToCartButton()
-        await expect(coffeePage.cartModal.miniCartContent()).toBeVisible()
-    });
+test.only('Add the first product item to the cart', async ({ page, homePage, coffeePage }) => {
+    await homePage.open();
+    expect(page.url()).toContain(await homePage.getPageUrl());
+    await homePage.header.clickOnFirstElement();
+    await expect (coffeePage.productItem.productItemList()).toContainText('Coffee Tonic');
+    await coffeePage.productItem.clickAddToCartButton();
+    await expect(coffeePage.cartModal.miniCartContent()).toBeVisible();
+});
