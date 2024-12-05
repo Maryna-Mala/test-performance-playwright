4fab75050e36059246b062f8fc23ce019e2c5e50
Maryna Mala <marinamalaa0@gmail.com>
2024-12-05 13:13:26 +0200

    changes

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -9,9 +9,7 @@ test.describe("Idealist login", async () => {
     await page.locator("#email").fill("marinamalaa0@gmail.com");
     await page.locator('[name="login[password]"]').fill("Testuser12345");
     await page.locator("//button[contains(@class, 'primary')]").click();
-    const errorMessage = await page
-      .locator('[data-ui-id="message-error"]')
-      .textContent();
+    const errorMessage = await page.locator('[data-ui-id="message-error"]').textContent();
 
     expect(errorMessage).toContain("Вхід в обліковий запис був неправильний");
   });
